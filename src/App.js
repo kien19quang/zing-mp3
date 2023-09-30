8ea30d2f56b7cc0912b22a6e732cfc8343907435
manh1111 <minhanh746347@gmail.com>
2023-09-30 21:10:04 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Manh <code>src/App.js</code> and save to reload.
+          Kieen ej <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
